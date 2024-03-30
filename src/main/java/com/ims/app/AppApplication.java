537efaab910a0fc26a7731d2760fe939c4224ff4
satvikkalyan <satvikkalyan@gmail.com>
2024-03-30 15:10:06 -0400

    Added .Env Files

--- src/main/java/com/ims/app/AppApplication.java
+++ src/main/java/com/ims/app/AppApplication.java
@@ -1,6 +1,7 @@
 package com.ims.app;
 
 import com.ims.app.config.userConfig.RSAKeyRecord;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(RSAKeyRecord.class)
 public class AppApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(AppApplication.class, args);
     }
 
